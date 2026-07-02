86aeed4d2c627ab0300f8ea477bbc0ab459f4ad0
txrxrxr <31713264+txrxrxr@users.noreply.github.com>
2026-07-01 21:18:00 -0700

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 1) {
+            return strs[0];
+        }
+
+        // search vertically by column
+        int lcp = 0;
+        for (int c = 0; c < strs[0].size(); ++c) {
+            for (int r = 1; r < strs.size(); ++r) {
+                if (c >= strs[r].size() || c >= strs[r-1].size()) {
+                    return strs[0].substr(0, c);
+                }
+                
+                if (strs[r][c] != strs[r-1][c]) {
+                    return strs[0].substr(0, c);
+                }
+
+            }
+        }
+
+        return strs[0];
+    }
+};
